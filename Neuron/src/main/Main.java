604ce98de477f5b25d16fff9e3bd572672b59824
Juan <koldur.c@gmail.com>
2018-12-03 09:52:32 +0100

    main page mockup

--- Neuron/src/main/Main.java
+++ Neuron/src/main/Main.java
@@ -1,9 +1,16 @@
 package main;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello there");
+		System.out.print("Do you want to train the model? [y/n]: ");
+		Scanner in = new Scanner(System.in);
+        String s = in.nextLine();
+        System.out.println("you selected " + s);
+        in.close();
 	}
 
 }
